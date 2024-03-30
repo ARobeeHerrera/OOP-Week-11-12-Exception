7acff9da8d008a3a092e3ffd29797715ff737d5b
Angelo Robee M. Herrera <152839434+ARobeeHerrera@users.noreply.github.com>
2024-03-30 17:56:41 +0800

    correctPassword.java

--- correctPassword.java
+++ correctPassword.java
@@ -1,6 +1,6 @@
 public class correctPassword extends Exception {
     public correctPassword(String password) throws WrongPasswordException {
-        String correctPassword = "Password";
+        String correctPassword = "password";
 
         if(!password.equals(correctPassword)) {
             throw new WrongPasswordException("Incorrect Password");
